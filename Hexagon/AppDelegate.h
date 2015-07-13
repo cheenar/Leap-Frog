febb0b12dc628bdd94467c5b997b133ad88921fc
Cheen <wff@l.com>
2015-07-12 22:15:28 -0400

    Initial Commit

---
+++ Hexagon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hexagon
+//
+//  Created by Cheenar Gupte on 7/12/15.
+//  Copyright (c) 2015 Chi Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hexagon/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Hexagon
+//
+
+//  Copyright (c) 2015 Chi Corp. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
